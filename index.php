338f0f61b46610aab474554bcfc7c2b80abf8e87
mirandaromario09 <mirandaromario09@gmail.com>
2026-05-06 13:39:53 -0300

    Corrige lógica das faixas de IR e adiciona return único no final da função

--- index.php
+++ index.php
@@ -14,23 +14,20 @@
 
         
 
-        function calcularIR ($salario){
+        function calcularIR($salario){
             $imposto = 0;
-            if ($salario <=1903.98)
+            if ($salario <= 1903.98){
                 $imposto = 0;
-            else if ($salario < 1903.99 && $salario > 2826.65){
+            } else if ($salario <= 2826.65){
                 $imposto = $salario * 0.075;
-            }
-            else if ($salario < 2826.66 && $salario > 3751.05){
+            } else if ($salario <= 3751.05){
                 $imposto = $salario * 0.15;
-            }
-            else if ($salario < 3751.06 && $salario > 4664.68){
+            } else if ($salario <= 4664.68){
                 $imposto = $salario * 0.225;
-            }
-            else if ($salario > 4664.68){
+            } else {
                 $imposto = $salario * 0.275;
-                return $imposto;
             }
+            return $imposto;
         }
 
         echo "Voce deverá pagar " . calcularIR(5000) . " de imposto!";
